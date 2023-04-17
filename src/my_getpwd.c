fe528252c214d0d5b3d6e0293d866a7d284b5682
Alban Peralta <alban.peralta@epitech.eu>
2023-04-18 01:01:48 +0200

    Add(my_getpwd.c): create a function to get the current pwd

---
+++ src/my_getpwd.c
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2022
+** 42sh
+** File description:
+** my_getpwd.c
+*/
+
+#include "mysh.h"
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
+// This function return the current working directory with a perfect sizeof.
+char *my_getpwd(void)
+{
+    char *tmp_pwd = malloc(sizeof(char) * 1024);
+    char *pwd = NULL;
+
+    getcwd(tmp_pwd, 1024);
+    pwd = strdup(tmp_pwd);
+    free(tmp_pwd);
+    return pwd;
+}
