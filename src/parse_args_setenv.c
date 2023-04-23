cedc2d8f53c077ca499121120192d5dccbe9d152
Alban Peralta <alban.peralta@epitech.eu>
2023-04-24 01:11:02 +0200

    Add(env management): create setenv and unsetenv

---
+++ src/parse_args_setenv.c
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2022
+** minishell
+** File description:
+** parse_args_setenv.c
+*/
+
+#include "my.h"
+#include "mysh.h"
+
+int parse_args_setenv(char **args)
+{
+    if (char_isnum(args[1][0]) == 1) {
+        my_puterror("setenv: Variable name must begin with a letter.\n");
+        return 1;
+    } if (str_isalphanum(args[1]) == 0) {
+        my_puterror("setenv: Variable name must"
+        " contain alphanumeric characters.\n");
+        return 1;
+    }
+    return 0;
+}
