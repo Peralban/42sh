f026d38c78bf413e4400d601082c315fb4355040
Alban Peralta <alban.peralta@epitech.eu>
2023-04-24 01:12:37 +0200

    Add(builtin.c): Create builtin function

---
+++ src/builtin.c
@@ -0,0 +1,36 @@
+/*
+** EPITECH PROJECT, 2022
+** 42sh
+** File description:
+** built-in.c
+*/
+
+#include "mysh.h"
+#include "my.h"
+#include <string.h>
+
+int built_in(char **cmd, char **env, int *error)
+{
+    (void) *error;
+    if (strcmp(cmd[0], "cd") == 0) {
+        // my_cd(cmd, env);
+        return 1;
+    }
+    if (strcmp(cmd[0], "env") == 0) {
+        print_array(env);
+        return 1;
+    }
+    if (strcmp(cmd[0], "setenv") == 0) {
+        my_setenv(cmd, env);
+        return 1;
+    }
+    if (strcmp(cmd[0], "unsetenv") == 0) {
+        my_unsetenv(cmd, env);
+        return 1;
+    }
+    if (strcmp(cmd[0], "exit") == 0) {
+        // my_exit(cmd, error);
+        return 1;
+    }
+    return 0;
+}
