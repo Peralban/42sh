cf335e4bbd980fee3137cf5ff91940bacf74a417
Alban Peralta <alban.peralta@epitech.eu>
2023-04-24 01:12:59 +0200

    Feat(all): modifying code to adapt it to my new functions

--- include/mysh.h
+++ include/mysh.h
@@ -9,5 +9,11 @@
     #define __mysh_H
 
 int main(int ac, char **av, char **env);
+int built_in(char **cmd, char **env, int *error);
+int parse_args_setenv(char **args);
+int my_unsetenv(char **args, char **env);
+int variable_only(char **env, char **args);
+int variable_and_value(char **env, char **args);
+int my_setenv(char **args, char **env);
 
 #endif
