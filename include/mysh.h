83796d51a8cbcd0419f9fa131772a4804eda85d4
Alban Peralta <alban.peralta@epitech.eu>
2023-04-28 14:46:04 +0200

    Add(header file, makefile): Add change directory function

--- include/mysh.h
+++ include/mysh.h
@@ -13,13 +13,14 @@
 int main(int ac, char **av, char **env);
 int built_in(char **cmd, char **env, int *error);
 int parse_args_setenv(char **args);
-int my_unsetenv(char **args, char **env);
-int my_setenv(char **args, char **env);
+int my_unsetenv(char **args, char **env, int *return_value);
+int my_setenv(char **args, char **env, int *return_value);
 int my_exit(char **cmd, int *error);
 char *my_getenv(char **env, char *var);
 int print_prompt(char **env, int result_cmd);
 char *my_getpwd(void);
 int setup_env(char **env);
 bool var_are_init(char **env);
+int my_cd(char **cmd, char **env, int *error);
 
 #endif
