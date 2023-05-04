05c09b2bdf14360be7f29b9fcab61158900dbeda
Thomaltarix <thomas.boue@epitech.eu>
2023-05-04 09:17:20 +0200

    Add(command_error_handling.c): Some free or array_destroy when needed

--- src/execution/command_error_handling.c
+++ src/execution/command_error_handling.c
@@ -52,10 +52,12 @@ static char *exist_in_path(char *cmd, char *path, int *error)
     for (int i = 0; path_tab[i] != NULL; i++) {
         tmp = get_tmp(path_tab[i], cmd);
         if (check_access(tmp, &not_access) == ACCESSABLE) {
+            destroy_array(path_tab);
             return tmp;
         }
         free(tmp);
     }
+    destroy_array(path_tab);
     if (not_access == true)
         my_put_permission_denied(cmd);
     return NULL;
@@ -76,8 +78,10 @@ char *search_command(char *cmd, char **env, int *error)
     }
     tmp = exist_in_path(cmd, path, error);
     if (tmp != NULL) {
+        free(path);
         return tmp;
     } else {
+        free(path);
         my_put_command_not_found(cmd);
         return NULL;
     }
