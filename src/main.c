54f1a482e3b81cea4b450e39d3ab8ff8e56c7206
Thomaltarix <thomas.boue@epitech.eu>
2023-05-04 09:15:59 +0200

    Add(main.c): The my_exec to the main loop

--- src/main.c
+++ src/main.c
@@ -50,7 +50,7 @@ static void loop(char **env_cpy)
         cmd = my_str_to_word_array(line, " \t");
         free(line);
         if (built_in(cmd, env_cpy, &error) != 2) {
-
+            my_exec(cmd, env_cpy, &error);
         }
         destroy_array(cmd);
     }
