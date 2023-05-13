661258ee0c9d290b06d4bd69310b4bf875169114
Aubane <aubane.nourry@epitech.eu>
2023-05-13 16:18:41 +0200

    Fix(local variables): Patch double variable definition

--- src/builtin/variable_builtins.c
+++ src/builtin/variable_builtins.c
@@ -36,12 +36,10 @@ void delete_local_variable(char *name)
 void append_local_variable(char *name, char *value)
 {
     char *path = get_path(".42sh_variables");
-    FILE *fd = fopen(path, "a");
+    FILE *fd = NONE;
 
-    if (fd == NULL)
-        return;
-    if (find_local_variable(name, 0) != NULL)
-        delete_local_variable(name);
+    delete_local_variable(name);
+    fd = fopen(path, "a");
     fprintf(fd, "%s %s\n", name, value);
     fclose(fd);
     free(path);
