ab795bae3ff9f439a2df4c9ff19ffe4272b39634
Thomaltarix <thomas.boue@epitech.eu>
2023-05-14 14:43:01 +0200

    Add (execute_alias.c): Work in progress

--- src/aliases/temporary/execute_alias.c
+++ src/aliases/temporary/execute_alias.c
@@ -11,18 +11,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-void execute_alias(char **cmd, char **env, int *error, int *exit_value)
+void execute_alias(char *line, int *error)
 {
-    char *alias_cmd = get_cmd_from_alias(env, cmd[0]);
-    char **alias_cmd_array = NULL;
-
-    if (alias_cmd == NULL)
-        return;
-    alias_cmd_array = malloc(sizeof(char *) * (my_arraylen(cmd) + 1));
-    alias_cmd_array[0] = strdup(alias_cmd);
-    for (int i = 1; cmd[i] != NULL; i++)
-        alias_cmd_array[i] = strdup(cmd[i]);
-    alias_cmd_array[my_arraylen(cmd)] = NULL;
-    exec_command(env, alias_cmd_array, error, exit_value);
     return;
 }
